cdc03c2fa1ee2162d577a648890e619c350fa707
DarkMatter015 <decamargoluk@gmail.com>
2025-10-19 12:09:10 -0300

    refactor(security): Harden and refactor entire JWT security layer

--- src/main/java/br/edu/utfpr/pb/ecommerce/server_ecommerce/security/dto/LoginRequestDTO.java
+++ src/main/java/br/edu/utfpr/pb/ecommerce/server_ecommerce/security/dto/LoginRequestDTO.java
@@ -1,17 +1,13 @@
 package br.edu.utfpr.pb.ecommerce.server_ecommerce.security.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class AuthenticationResponse {
-
-    private String token;
-    private SecurityUserResponseDTO user;
-
+@AllArgsConstructor
+public class LoginRequestDTO {
+    private String username;
+    private String password;
 }
